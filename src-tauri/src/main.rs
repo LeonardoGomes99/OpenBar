bf356b3632b204255196440bf8de82a43258cae0
LeonardoGomes99 <leogomes16477@gmail.com>
2023-05-13 16:27:23 -0300

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,50 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use tauri_plugin_log::{LogTarget};
 
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-
-use tauri::Manager;
-use window_shadows::set_shadow;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn open_executable(path: String) -> Result<(), String> {
-  std::process::Command::new(path)
-    .spawn()
-    .map_err(|err| format!("Failed to start executable: {}", err))?;
-  Ok(())
+    std::process::Command::new(path)
+        .spawn()
+        .map_err(|err| format!("Failed to start executable: {}", err))?;
+    Ok(())
 }
 
-fn main() {
+#[tauri::command]
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+#[tauri::command]
+fn open_file() -> Result<String, String> {
+    // Abre o arquivo em modo de leitura
+    let mut file = match File::open("config.json") {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(format!("Erro ao abrir o arquivo: {}", err));
+        }
+    };
+
+    let mut contents: String = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => Ok(contents),
+        Err(err) => Err(format!("Erro ao ler o arquivo: {}", err)),
+    }
+}
+
+fn main() {    
+    // let file_contents = open_myfile();
+    // let jsonSting = file_contents.unwrap();
+    // println!("{}", jsonSting);
+
     tauri::Builder::default()
-        .setup(|app| {
-            let window = app.get_window("main").unwrap();
-            set_shadow(&window, true).expect("Unsupported platform!");
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            // ... outros comandos aqui
-            open_executable
-        ])
+        .invoke_handler(tauri::generate_handler![open_executable, open_file])
+        .plugin(tauri_plugin_log::Builder::default().targets([
+          LogTarget::LogDir,
+          LogTarget::Stdout,
+          LogTarget::Webview,
+      ]).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import { useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+import { listen } from '@tauri-apps/api/event'
+
+import { trace, info, error, attachConsole } from "tauri-plugin-log-api";
+
+// with LogTarget::Webview enabled this function will print logs to the browser console
+const detach = await attachConsole();
+
 function App() {
 
-  function openExecutable() {
+  const [icons, setIcons] = useState([])
+
+  const openExecutable = (path: any) => {
     invoke("open_executable", {
-      path: "C:\\Program Files (x86)\\Steam\\steam.exe"
+      path: path
     });
   }
 
+  useEffect(() => {
+    const printFile = async () => {
+      let json = await invoke("open_file") as any;
+      setIcons(JSON.parse(json))
+    }
+    printFile()
+  }, [])
+
   return (
-    <div className="container">      
-      <div className="row">
-        <a onClick={openExecutable}>
-          <img src="/steam.svg" className="logo steam" alt="Steam Logo" />
-        </a>        
-      </div>      
+    <div className="container">
+      <div className="row">        
+        {
+          icons.map((x: any) => {
+            return (
+              <Fragment>
+                <a onClick={() => openExecutable(x?.path)}>
+                  <img src={`/${x.icon}`} className="logo steam" alt="Steam Logo" />
+                </a>
+              </Fragment>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
